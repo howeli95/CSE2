843db66aa80268e6b67bf66c37b53642d4fc200e
howeli95 <yul217@lehigh.edu>
2015-08-30 19:04:59 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,31 @@
+//Yunhao LI
+//hw 01
+//WelcomeClass Java Program
+//
+//  first compile the program
+//      javac WelcomeClass.java
+//  run the program
+//      java Welcome Class//
+
+//  define a class
+public class WelcomeClass{
+    
+//add main method
+    public static void main(String[] args) {
+        
+//  print the statement with box around it
+    System.out.println("  -----------");
+    System.out.println("  | WELCOME |");
+    System.out.println("  -----------");
+    System.out.println("  ^  ^  ^  ^  ^  ^");
+    System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\");
+    System.out.println("|-Y--U--L--2--1--7-|");
+    System.out.println(" \\/\\ /\\ /\\ /\\ /\\ /");
+    System.out.println("  v  v  v  v  v  v");
+    System.out.println("This summer I participated in Mountaintop");
+    System.out.println("Program with Arch professor");
+
+    
+    //test/testtest
+    }
+}
