ba5277e5f86c223ba0c8f4a4bc495909c3ca8db3
howeli95 <yul217@lehigh.edu>
2015-10-04 00:53:52 +0000

    updated hw06

--- exercise/sept30.java
+++ exercise/sept30.java
@@ -4,15 +4,15 @@
 public class sept30{
     public static void main(String [] arugs){
         
-        for(int i = 1; i <= 10; i++){
+        /*for(int i = 1; i <= 10; i++){
             System.out.print(i);
             if (i % 3 == 0)
             break;
-        }
+        }*/
         
      /*   for(char x = 'a'; x < 'z'; x++){
             System.out.print(x);
         }*/
-        
-    }
+
+}
 }
---
+++ hw06/pyramid.java
@@ -0,0 +1,26 @@
+//Yunhao Li
+//Oct. 3, 2015
+//CSE 002
+//HW 06 - pyramid program
+
+import java.util.Scanner;
+
+public class pyramid{ //define a class
+    public static void main(String[] arug){ //define a method
+        Scanner myScanner = new Scanner(System.in);
+        //ask user to enter size of the pyramid
+        System.out.print("What size pyramid? ");
+        double s = myScanner.nextDouble();
+        
+        for(int a=0;a<s;a++){
+            for(int b=0; b<=s-a;b++){
+                System.out.print(" ");
+            }
+            for (double c=0;c<=a;c+=0.5){
+                System.out.print("*");
+            }
+        System.out.println();
+            
+        }
+    }//end of the method
+}//end of the class
